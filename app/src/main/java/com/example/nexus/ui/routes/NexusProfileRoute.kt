8c18887478f151c3eddf73bad2f477006b0096cc
avoyen <alexander.vanoyne@ugent.be>
2022-04-15 21:51:35 +0200

    bottom navigation bar

--- app/src/main/java/com/example/nexus/ui/routes/NexusProfileRoute.kt
+++ app/src/main/java/com/example/nexus/ui/routes/NexusProfileRoute.kt
@@ -1,12 +1,45 @@
 package com.example.nexus.ui.routes
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.nexus.viewmodels.NexusProfileViewModel
 
 @Composable
-fun NexusProfileRoute(
-    vM: NexusProfileViewModel
-){
-    Text(text = "profile")
+fun NexusProfileRoute(vM: NexusProfileViewModel){
+    NexusProfileScreen(
+
+    )
 }
+
+
+@Composable
+fun NexusProfileScreen(){
+    Scaffold( topBar = {
+        TopAppBar(
+            title = { Text("Profile") },
+            )
+        Column (
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.End
+        ){
+            IconButton(
+                onClick = {/* TODO */}
+                , modifier = Modifier.
+                then(Modifier.size(58.dp))
+            ) {
+                Icon(Icons.Default.Settings, "Settings")
+            }
+
+        }
+    }) {
+
+    }
+}
+
--- app/src/main/java/com/example/nexus/viewmodels/NexusFriendsViewModel.kt
+++ app/src/main/java/com/example/nexus/viewmodels/NexusFriendsViewModel.kt
@@ -2,7 +2,6 @@ package com.example.nexus.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.example.nexus.data.FriendsRepository
-import com.example.nexus.data.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
