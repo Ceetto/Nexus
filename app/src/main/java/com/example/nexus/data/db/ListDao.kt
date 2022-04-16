9c44d5f23db39655aa3ec521330abd30f62b268c
Sheng Tao <shengtao.tian@ugent.be>
2022-04-16 20:06:34 +0200

    #1 queries toegevoegd aan ListDao.kt

--- app/src/main/java/com/example/nexus/data/db/ListDao.kt
+++ app/src/main/java/com/example/nexus/data/db/ListDao.kt
@@ -5,24 +5,25 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.nexus.data.web.ListEntry
+import com.example.nexus.ui.routes.list.ListCategory
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ListDao {
 
-    @Query("")
+    @Query("select * from list")
     fun getAll(): Flow<List<ListEntity>>
 
-    @Query("")
+    @Query("select * from list where status == 'Playing'")
     fun getPlaying(): Flow<List<ListEntity>>
 
-    @Query("")
+    @Query("select * from list where status == 'Completed'")
     fun getCompleted(): Flow<List<ListEntity>>
 
-    @Query("")
+    @Query("select * from list where status == 'Planned'")
     fun getPlanned(): Flow<List<ListEntity>>
 
-    @Query("")
+    @Query("select * from list where status == 'Dropped'")
     fun getDropped(): Flow<List<ListEntity>>
 
     @Insert(entity = ListEntity::class)
--- app/src/main/java/com/example/nexus/data/db/ListEntity.kt
+++ app/src/main/java/com/example/nexus/data/db/ListEntity.kt
@@ -4,12 +4,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.nexus.ui.routes.list.ListCategory
 
-@Entity
+@Entity(tableName = "list")
 data class ListEntity (
     @PrimaryKey(autoGenerate = true) val id: Int,
     val gameId: Int,
     val title: String,
     val score: Int,
     val hoursPlayed: Int,
-    val status: ListCategory
+    val status: String
 )
