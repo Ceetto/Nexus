768209b31725721430b4f14c0056a410039a038e
Sheng Tao <shengtao.tian@ugent.be>
2022-05-07 15:21:21 +0200

    generic functie toevoegd om ongesorteerd games te krijgen van de ListRepository.kt

--- app/src/main/java/com/example/nexus/viewmodels/NexusProfileViewModel.kt
+++ app/src/main/java/com/example/nexus/viewmodels/NexusProfileViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.nexus.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.nexus.data.dataClasses.ListEntry
+import com.example.nexus.data.repositories.ListRepository
 import com.example.nexus.data.repositories.ProfileRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class NexusProfileViewModel @Inject constructor(private val repo: ProfileRepository) : ViewModel(){
+class NexusProfileViewModel @Inject constructor(private val profileRepo: ProfileRepository, private  val listRepo: ListRepository) : ViewModel(){
+
+    fun getCategoryByName(category: String): StateFlow<List<ListEntry>> {
+        return listRepo.getCategoryByName(category).stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+    }
 }
