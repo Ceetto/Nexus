47c37130dd01fe4681ac6d8dc390368379dc35f0
avoyen <alexander.vanoyen@ugent.be>
2022-05-14 21:13:27 +0200

    friend profile

--- app/src/main/java/com/example/nexus/data/db/FirebaseUserDao.kt
+++ app/src/main/java/com/example/nexus/data/db/FirebaseUserDao.kt
@@ -2,6 +2,7 @@ package com.example.nexus.data.db
 
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
+import com.example.nexus.data.dataClasses.Friend
 import com.example.nexus.data.dataClasses.User
 import com.example.nexus.data.dataClasses.getUserId
 import com.google.firebase.auth.FirebaseAuth
@@ -16,16 +17,19 @@ import javax.inject.Singleton
 class FirebaseUserDao @Inject constructor(
     private val database: FirebaseDatabase,
     private val auth: FirebaseAuth
-
 ){
-    private val userId = mutableStateOf(getUserId(auth.currentUser))
+    private val friendId = mutableStateOf("")
 
-    private val userRef = mutableStateOf(database.getReference("user/${userId.value}"))
+    private val userRef = mutableStateOf(database.getReference("user/${getUserId(auth.currentUser)}"))
+
+    private val friendRef = mutableStateOf(database.getReference("user/${friendId.value}"))
 
     private val newUser = mutableStateOf(User("",
         "NewUser",
         "",
         "",))
+
+    private val newFriend = mutableStateOf(User( "", "", "", ""))
 
     private val eventListener =
         object: ValueEventListener {
@@ -34,47 +38,69 @@ class FirebaseUserDao @Inject constructor(
 //                 This method is called once with the initial value and again
 //                 whenever data at this location is updated.
             if (snapshot.children.count() >= 4  && snapshot.child("email").value != null) {
-                println("in de if statement")
                 newUser.value.email = snapshot.child("email").value as String
                 newUser.value.username = snapshot.child("username").value as String
                 newUser.value.profilePicture = snapshot.child("profilePicture").value as String
                 newUser.value.profileBackground = snapshot.child("profileBackground").value as String
             }
-            println("current user id = ${getUserId(auth.currentUser)}")
-            println("newuser emial = " + newUser.value.email)
-            println("newuser username = " + newUser.value.username)
-
         }
 
         override fun onCancelled(error: DatabaseError) {
             Log.w(TAG, "Failed to read value.", error.toException())
         }
-
-
     }
+
+
+    private val eventListenerFriend =
+        object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+//                 This method is called once with the initial value and again
+//                 whenever data at this location is updated.
+                if (snapshot.children.count() >= 4 ) {
+                    newFriend.value.email = snapshot.child("email").value as String
+                    newFriend.value.username = snapshot.child("username").value as String
+                    newFriend.value.profilePicture = snapshot.child("profilePicture").value as String
+                    newFriend.value.profileBackground = snapshot.child("profileBackground").value as String
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        }
+
+
+
+
+
+
 
     private val realtimeEntries = mutableStateOf(userRef.value.addValueEventListener(eventListener))
 
     fun setUserid(id: String) {
-        if (id == "") {
-            userId.value = getUserId(auth.currentUser)
-        } else {
-            userId.value = id
-        }
-        println("new user id = $id")
-        updateUser()
+        friendId.value = id
+        updateFriend()
     }
 
     fun updateUser(){
-        userRef.value = database.getReference("user/${userId.value}")
-        println("user/${userId.value}")
+        userRef.value = database.getReference("user/${getUserId(auth.currentUser)}")
         realtimeEntries.value = userRef.value.addValueEventListener(eventListener)
+    }
+
+    fun updateFriend(){
+        friendRef.value = database.getReference("user/${friendId.value}")
+        friendRef.value.addValueEventListener(eventListenerFriend)
     }
 
     private val TAG = "UserRepository"
 
     fun getUser() : User{
         return newUser.value
+    }
+
+    fun getFriend() : User {
+        return newFriend.value
     }
 
     fun storeNewUser(user : User)  {
